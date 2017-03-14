9bba8052a16a1e68b6057523113176dc0af429f8
joemaidman <joemaidman@gmail.com>
2017-03-14 16:27:24 +0000

    Testing outcome of isSunny

--- jasmine/spec/weatherSpec.js
+++ jasmine/spec/weatherSpec.js
@@ -1,26 +1,26 @@
 describe("Weather", function() {
   var weather;
 
-  it ("Responds to sunny?", function() {
+  beforeEach(function() {
     weather = new Weather;
-    expect(weather.isSunny(weather._randNum())).toBeDefined();
+  })
+
+  it ("Responds to sunny?", function() {
+    expect(weather.isSunny).toBeDefined();
   });
 
   it ("returns true or false", function() {
-    weather = new Weather;
-    expect([true, false]).toContain(weather.isSunny(weather._randNum()));
+    expect([true, false]).toContain(weather.isSunny());
   });
 
   it ("returns true when 100% chance of being sunny", function() {
-    weather = new Weather;
-    // var spy = spyOn(Math, 'random').and.returnValue(0.1);
-    // expect(weather.isSunny).toEqual(true);
-    expect(weather.isSunny(0)).toEqual(true);
+    spyOn(Math, 'random').and.returnValue(0);
+    expect(weather.isSunny()).toEqual(true);
   });
 
   it ("returns false when 100% chance of not being sunny", function() {
-    weather = new Weather;
-    expect(weather.isSunny(1)).toEqual(false);
+    spyOn(Math, 'random').and.returnValue(1);
+    expect(weather.isSunny()).toEqual(false);
   });
 
 });
--- jasmine/src/weather.js
+++ jasmine/src/weather.js
@@ -1,9 +1,5 @@
 var Weather = function(){};
 
-Weather.prototype.isSunny = function(num) {
-  return num < 0.8;
-}
-
-Weather.prototype._randNum = function() {
-  return Math.random();
+Weather.prototype.isSunny = function() {
+  return Math.random() < 0.8;
 }
